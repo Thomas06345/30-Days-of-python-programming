ca6724d922c7eda886d7076b7adb949ae795fe02
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-09 22:32:16 +0530

    To find is given number is positive, negative or zero

---
+++ positive negative day2.py
@@ -0,0 +1,12 @@
+'''Author:Thomas Cherian
+   date: 9-11-24
+   python program:To check whether given number is positive negative or zero.'''
+num=int(input("enter your number:"))
+if num>0:
+	print("The number is positive")
+elif num<0:
+	print("The number is negative")
+else:
+	print("The number is zero")
+	
+		
