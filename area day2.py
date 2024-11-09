4854253ffb385eac015bbd099c793b89d97a8648
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-09 22:32:43 +0530

    To find area of square

---
+++ area day2.py
@@ -0,0 +1,7 @@
+'''Author:Thomas Cherian
+   date: 9-11-24
+   python program:To print area of a rectangle .'''
+length=float(input("Enter length:"))
+width=float(input("Enter width:"))
+area=length*width
+print("The area is ",area)
