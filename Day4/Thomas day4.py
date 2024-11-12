f9ff409dacf669db0700a9c3fe12a226cc4dce47
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-12 21:10:10 +0530

    To produce the length of a given string

---
+++ Day4/Thomas day4.py
@@ -0,0 +1,6 @@
+'''''Author:Thomas Cherian
+   date: 12-11-24
+   python program:To produce the length of a given string.'''
+string=input("Enter string:")
+length=len(string)
+print(f"The length of the string {string} is {length}")
