dc3fcadc5c925eea94f725caf890c09d2478af06
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-12 21:11:32 +0530

    To add seperate strings to create a single string

---
+++ Day4/Thomas(2) day4.py
@@ -0,0 +1,7 @@
+'''Author:Thomas Cherian
+   date: 12-11-24
+   python program:To add to seperate strings to create a single string .'''
+string1=input("Enter first string:")
+string2=input("Enter second string:")
+string3=string1+string2
+print(f"The string obtained after adding {string1} and {string2} is {string3}")
