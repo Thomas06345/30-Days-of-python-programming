8a7903ba13a5966fcb1e21aacd7947871574842d
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-16 23:35:53 +0530

    To check if a num is positive negative or zero using function

---
+++ Day-6/Thomas(2) day6.py
@@ -0,0 +1,13 @@
+'''Author:Thomas Cherian
+   date: 16-11-24
+   python program:To check is a num is positive negative or zero using function.'''
+def check(num):
+	if num>0:
+		print(f"The number {num} is positive ")
+	elif num<0:
+		print(f"The number {num} is negative ")
+	else :
+			print(f"The number is zero")
+
+check(-2)
+			
