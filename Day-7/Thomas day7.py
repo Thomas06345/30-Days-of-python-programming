68cab4d8d8440eeddf415e10b8a884a7464a09bb
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-17 22:46:22 +0530

    To print a list using list function.

---
+++ Day-7/Thomas day7.py
@@ -0,0 +1,6 @@
+'''Author:Thomas Cherian
+   date: 17-11-24
+   python program:To print a list using list function'''
+food=["pizza","burger","ice cream","roti","dosa"]
+for food in food:
+	print(food)
