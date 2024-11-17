4d13266e8d85e06043158d503fd205da81ff02c0
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-17 22:47:09 +0530

    To add a new item to list using list function and print it

---
+++ Day-7/Thomas(2) day7.py
@@ -0,0 +1,7 @@
+'''Author:Thomas Cherian
+   date: 17-11-24
+   python program:To add a new item to list using list function and print it'''
+food=["pizza","burger","ice cream","roti","dosa"]
+food.append("sandwitch")
+for food in food:
+	print(food)
