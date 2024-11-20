c5a5a922e7685291bcb94c09b1343f70d0621959
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-11-20 21:58:04 +0530

    Add files via upload

---
+++ Day-6/vismaya6(2).py
@@ -0,0 +1,13 @@
+'''
+Author:Vismaya Theresa Benny
+Date:20/11/2024
+python program to check whether the number is positive,negaive or zero
+'''
+def check(number):
+    if number>0:
+        print("The number",number,"is positive")
+    elif number<0:
+        print("The number",number,"is negative")
+    else:
+        print("The given number",number,"is zero")
+check(6)
