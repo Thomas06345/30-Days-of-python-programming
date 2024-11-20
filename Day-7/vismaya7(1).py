31394bd46f7a021b08161f02a8960fcb4096da8d
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-11-20 21:58:31 +0530

    Add files via upload

---
+++ Day-7/vismaya7(1).py
@@ -0,0 +1,8 @@
+'''
+Author:Vismaya Theresa Benny
+Date:20/11/2024
+python program to crate a list and print each item using a loop
+'''
+Food=["Pizza","Sushi","Burger","Pasta","Icecream"]
+for f in Food:
+    print(f)
