d75494bf5e8eefac4d7fcf08b7e667839ed42de3
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-20 23:42:47 +0530

    To print a set of names using list

---
+++ Day-9/Thomas(2) day9.py
@@ -0,0 +1,6 @@
+'''Author:Thomas Cherian
+   date: 19-11-24
+   python program:To print a set of names using list '''
+friends=["Kenaz","Daiji","Sanjay","Aeby"]
+for friends in friends:
+	print(friends)
