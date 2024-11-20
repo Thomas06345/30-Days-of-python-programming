731db1fcbe8c74159de48ffdc48896f39cb67d9e
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-11-20 21:58:57 +0530

    Add files via upload

---
+++ Day-7/vismaya7(2).py
@@ -0,0 +1,10 @@
+'''
+Author:Vismaya Theresa Benny
+Date:20/11/2024
+python program to create a list and add a new item to it
+'''
+
+Food=["Pizza","Sushi","Burger","Pasta","Icecream"]
+Food.append("Tacos")
+for f in Food:
+    print(f)
