8e29912a726a0d61b30b458c347de20e69c6606a
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-21 23:40:07 +0530

    To check if given number is odd or even

---
+++ Day-11/Thomas day11.py
@@ -0,0 +1,8 @@
+'''Author:Thomas Cherian
+   date: 21-11-24
+   python program:To check if given number is odd or even '''
+num=int(input("Enter your number:"))
+if num % 2 == 0:
+	print("The number is even")
+else:
+		print("The number is odd")
