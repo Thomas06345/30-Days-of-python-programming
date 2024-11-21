c89c260d0c7b926c296ab020c966ac9fe9aded6d
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-21 23:40:44 +0530

    To check if the user input is greater than 10

---
+++ Day-11/Thomas(2) day11.py
@@ -0,0 +1,8 @@
+'''Author:Thomas Cherian
+   date: 21-11-24
+   python program:To check if the user input is greater than 10 '''
+num=int(input("Enter a number":))
+if num > 10:
+	print("Number is greater than 10")
+else:
+	print("Number is less than 10")
