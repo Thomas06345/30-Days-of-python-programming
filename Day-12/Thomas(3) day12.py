15289cf41c461ef1ccf52d533734b4b529b08108
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-27 20:29:30 +0530

    To print single values using tuple unpacking

---
+++ Day-12/Thomas(3) day12.py
@@ -0,0 +1,8 @@
+'''Author:Thomas Cherian
+   date: 25-11-24
+   python program:To print single values using tuple unpacking '''
+info=("Thomas","Cherian",19)
+first_name,last_name,age=info 
+print(first_name)
+print(last_name)
+print(age)
