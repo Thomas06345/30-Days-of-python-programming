231294476949e55c113a48f87db7a87e66daea14
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-28 19:57:41 +0530

    To use set membership.

---
+++ Day-13/Thomas(3) day13.py
@@ -0,0 +1,9 @@
+'''''Author:Thomas Cherian
+   date: 26-11-24
+   python program:To use set membership.'''
+fruits={"apple","banana","strawberry","dragonfruits"}
+print(fruits)
+fruits.add("apple")
+print(fruits)
+print("apple" in fruits)
+print("orange" in fruits)
