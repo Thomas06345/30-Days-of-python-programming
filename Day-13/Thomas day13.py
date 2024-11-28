0bb40a0e58760f515c6bd84a5147b6891c6fd25a
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-28 19:55:26 +0530

    To print a set

---
+++ Day-13/Thomas day13.py
@@ -0,0 +1,7 @@
+'''''Author:Thomas Cherian
+   date: 26-11-24
+   python program:To print a set .'''
+fruits={"apple","banana","strawberry","dragonfruits"}
+print(fruits)
+fruits.add("apple")
+print(fruits)
