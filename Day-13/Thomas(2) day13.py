10cfb0e05f655a3d9a1d19e47b702d4b7c75c413
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-28 19:56:18 +0530

    To use union intersection and difference

---
+++ Day-13/Thomas(2) day13.py
@@ -0,0 +1,11 @@
+'''''Author:Thomas Cherian
+   date: 26-11-24
+   python program:To use union intersection and difference.'''
+odd = {1,3,5,7,9}
+even= {2,4,6,8,10}
+union = odd | even
+intersection = odd & even
+difference = odd - even 
+print(union)
+print(intersection)
+print(difference)
