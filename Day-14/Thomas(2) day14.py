0746c2bd0597b90d44b74c6e4fe839ca12b89ae8
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-29 19:18:39 +0530

    To add a new value to the dictionary

---
+++ Day-14/Thomas(2) day14.py
@@ -0,0 +1,6 @@
+'''''Author:Thomas Cherian
+   date: 27-11-24
+   python program:To add a new value to the dictionary.'''
+person ={"name": "Thomas Cherian", "age": "18", "city":"kottayam"}
+person["favorite_colour"]="blue"
+print(person)
