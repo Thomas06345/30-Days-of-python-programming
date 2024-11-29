958cbe173de678cf8da5b3870f66f70646966c52
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-29 19:15:59 +0530

    To create and print a dictionary

---
+++ Day-14/Thomas day14.py
@@ -0,0 +1,10 @@
+'''''Author:Thomas Cherian
+   date: 27-11-24
+   python program:To create and print a dictionary.'''
+person ={"name": "Thomas Cherian", "age": "18", "city":"kottayam"}
+print(person)
+print(person["name"])
+print(person["age"])               
+print(person["city"])                
+               
+             
