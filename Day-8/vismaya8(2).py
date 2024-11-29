6b1692c8d98318f62ba8ae1f4d1dd64a8e1b7c03
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-11-29 23:54:34 +0530

    Add files via upload

---
+++ Day-8/vismaya8(2).py
@@ -0,0 +1,8 @@
+'''
+Author:Vismaya Theresa Benny
+Date:29/11/2024
+python prrogram to print even numbers using list slicing
+'''
+numbers=[1,2,3,4,5,6,7,8]
+even=numbers[1::2]
+print("even numbers:",even)
