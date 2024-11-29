a8fa1d295727ad46a8017b5523331ee1159c13d4
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-11-29 23:56:53 +0530

    Add files via upload

---
+++ Day-11/vismaya11(2).py
@@ -0,0 +1,12 @@
+'''
+Author:Vismaya Theresa Benny
+Date:29/11/2024
+python prrogram to check the number is greater than 10 or not
+'''
+num=int(input("Enter the number:"))
+if num>10:
+    print("greater")
+elif num<10:
+    print("lesser")
+else:
+    print("Equal")
---
+++ Day-11/vismaya11.py
@@ -0,0 +1,10 @@
+'''
+Author:Vismaya Theresa Benny
+Date:29/11/2024
+python prrogram to check a number is even or odd
+'''
+number=int(input("Enter a number:"))
+if number/2==0:
+    print("The given no is even")
+else:
+    print("odd")
