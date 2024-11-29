8e1108bb88bd46886c21ce788fe98c48875cb36a
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2024-11-29 23:55:32 +0530

    Add files via upload

---
+++ Day-9/vismaya9(2).py
@@ -0,0 +1,8 @@
+'''
+Author:Vismaya Theresa Benny
+Date:29/11/2024
+python prrogram to print names ina list using fr loop
+'''
+names=["Ann","Mary","Maria"]
+for i in names:
+    print(i)
---
+++ Day-9/vismaya9.py
@@ -0,0 +1,7 @@
+'''
+Author:Vismaya Theresa Benny
+Date:29/11/2024
+python prrogram to print numbers from 1 to 10 using for loop
+'''
+for num in range(1,11):
+    print(num)
