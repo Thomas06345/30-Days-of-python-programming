b95da4e1976ca1494c15d0acef31e9930c2bc616
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-29 23:25:33 +0530

    To update the value

---
+++ Day-15/Thomas day15.py
@@ -0,0 +1,6 @@
+'''''Author:Thomas Cherian
+   date: 29-11-24
+   python program:To update the value.'''
+person ={"name": "Thomas Cherian", "age": "18", "city":"kottayam"}
+person["age"]=19
+print(person)
