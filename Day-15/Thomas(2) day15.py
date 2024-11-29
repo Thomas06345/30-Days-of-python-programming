187fd5775df4804a7ecfd5805650540e2b30f705
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-11-29 23:26:16 +0530

    To print the keys in the dictionary

---
+++ Day-15/Thomas(2) day15.py
@@ -0,0 +1,6 @@
+'''''Author:Thomas Cherian
+   date: 29-11-24
+   python program:To print the keys in the dictionary.'''
+person ={"name": "Thomas Cherian", "age": "18", "city":"kottayam"}
+for key in person.keys():
+	print(key)
